3e29489500d02910fd97da1c8644e87bb890ccd3
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-04-25 15:50:44 -0300

    add API, fetch data, render data from API, style the cards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.css';
+import displayMovies from './modules/displayCards.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  displayMovies();
+});
---
+++ src/modules/displayCards.js
@@ -0,0 +1,24 @@
+import movies from './moviesApi.js';
+
+const displayMovies = async () => {
+  const moviesContainer = document.getElementById('displayCards');
+  const moviesData = await movies();
+  const moviesHtml = moviesData.results.map((movie) => `
+    <div class="movie-card ${movie.id}">
+      <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="poster">
+      <div class="moviesTitle">
+        <h3 class="movieTitle">${movie.title}</h3>
+        <button class="like-btn btn" id="like${movie.id}">
+        <i class="fa-sharp fa-solid fa-heart"><span class="like-count">0</span></i>
+        </button>
+      </div>
+      <p class="overview">${movie.overview}</p>
+      <div class="button-container">
+        <button data-modal-target="#card${movie.id}" type="button" class="comments" id="comments">Comments</button>
+      </div>
+    </div>
+  `).join('');
+  moviesContainer.innerHTML = moviesHtml;
+};
+
+export default displayMovies;
