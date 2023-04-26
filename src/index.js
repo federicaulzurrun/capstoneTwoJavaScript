f1c561d5d553a4ddab2c2007add988c187f283c4
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-04-26 11:54:02 -0300

    trying code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 import displayMovies from './modules/displayCards.js';
+import likeButtons from './modules/counterLikes.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   displayMovies();
+  likeButtons();
 });
---
+++ src/modules/counterLikes.js
@@ -0,0 +1,26 @@
+// import { response } from "express";
+
+// Involment API link
+// const likesUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Ir4GAcPLgchx7TvyAEZT/likes';
+
+// const getLikes = async () => {
+//   const cardMovie = document.querySelectorAll('.movie-card');
+//   const likeCount = document.querySelectorAll('.like-count');
+//   await fetch(likeCount)
+//     .then((response) => response.json())
+//     .then((json) => {
+//       const moviesArr = Array.from(cardMovie);
+//       moviesArr.forEach((card, index) => {
+//         json.forEach((item) => {
+//           if (item.item_id === card.id) {
+//             likeCount[index].innerHTML = item.likes;
+//           }
+//         });
+//       });
+//     });
+// };
+
+// const addingLikes = async () => {
+//   const likeCount = document.querySelectorAll('.like-count');
+
+// };
